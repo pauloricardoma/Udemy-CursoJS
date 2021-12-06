70ccfdd323ca5f794e7876ea47e8d0f889ebd0ec
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2021-12-06 00:21:11 -0300

    mais aulas

--- Módulo09-JS-NodeJS/aula82-MongoDB-Conexao-PrimeiroModel/src/controllers/homeController.js
+++ Módulo09-JS-NodeJS/aula82-MongoDB-Conexao-PrimeiroModel/src/controllers/homeController.js
@@ -1,3 +1,16 @@
+const HomeModel = require('../models/HomeModel');
+
+// HomeModel.find()
+//   .then((dados) => console.log(dados))
+//   .catch((e) => console.log(e));
+
+// HomeModel.create({
+//   titulo: 'Outra coisa qualquer',
+//   descricao: 'Outra descrição',
+// })
+//   .then((dados) => console.log(dados))
+//   .catch((e) => console.log(e));
+
 exports.paginaInicial = (req, res) => {
   res.render('index');
   return;
---
+++ Módulo09-JS-NodeJS/aula83-ExpressSession-FlashMessages/src/controllers/homeController.js
@@ -0,0 +1,16 @@
+exports.paginaInicial = (req, res) => {
+  // req.flash('info', 'Olá mundo!');
+  // req.flash('error', 'sadsadlasld!');
+  // req.flash('sucess', 'Blaaaaaaaaaaaaaaaaaaaaa');
+  console.log(req.flash('error'), req.flash('sucess'), req.flash('info'));
+
+  // req.session.usuario = { nome: 'Luiz', logado: true };
+  // console.log(req.session.usuario);
+  res.render('index');
+  return;
+};
+
+exports.trataPost = (req, res) => {
+  res.send(req.body);
+  return;
+};
