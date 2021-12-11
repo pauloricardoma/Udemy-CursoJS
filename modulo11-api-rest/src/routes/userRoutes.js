5b5be8505f69840bfb8c43af8715a20877d55c82
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2021-12-11 13:51:05 -0300

    inicio projeto REST

---
+++ modulo11-api-rest/src/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+
+export default router;
+
+/*
+Padrões a serem seguidos na criação do controller
+index -> lista todos os usuários -> GET
+store/create -> cria um novo usuário -> POST
+delete -> apaga um usuário -> DELETE
+show -> mostra um usuário -> GET
+update -> atualiza um usuário -> PATCH ou PUT
+*/
